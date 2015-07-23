cae4e9972553a6d92627d063fabbcfa48844ac82
Alexandre Bailon <abailon@baylibre.com>
2015-07-23 10:22:25 +0200

    Do some cleanup
    Remove some duplicated code and fix some withspace issues.
    
    Signed-off-by: Alexandre Bailon <abailon@baylibre.com>

--- gb-loopback.c
+++ gb-loopback.c
@@ -23,6 +23,16 @@
 #include <string.h>
 #include <sys/time.h>
 
+struct loopback
+{
+	int cport;
+	int type;
+	int size;
+	int ms;
+
+	int monitor;
+};
+
 volatile int run = 1;
 
 void sig_handler(int signo)
@@ -42,42 +52,128 @@ int get_cportid(const char *path)
 	return cport;
 }
 
-int main (int argc, char *argv[])
+void loopback_monitor(struct loopback *loopback, struct udev_device *dev,
+		      int cportid)
 {
-	int type = 0;
-	int size = 0;
-	int cport = -1;
-	int monitor = 0;
-	int ms = -1;
-	struct udev *udev;
-	struct udev_enumerate *enumerate;
-	struct udev_list_entry *devices, *dev_list_entry;
-	struct udev_device *dev;
+	int type = loopback->type;
+
+	printf("cport %d\n", cportid);
+	printf("\tfreq = %s\n",
+		udev_device_get_sysattr_value(dev, "frequency_avg"));
+	printf("\tlatency = %s\n",
+		udev_device_get_sysattr_value(dev, "latency_avg"));
+	printf("\tbw = %s\n",
+		udev_device_get_sysattr_value(dev, "throughput_avg"));
+	printf("\terrors = %s\n",
+		udev_device_get_sysattr_value(dev, "error"));
+	if (run == 2 && type)
+		udev_device_set_sysattr_value(dev, "type", "0");
+
+}
+
+void loopback_configure(struct loopback *loopback, struct udev_device *dev,
+			int cportid)
+{
 	char value[32];
+	int type = loopback->type;
+	int size = loopback->size;
+	int monitor = loopback->monitor;
+	int ms = loopback->ms;
+
+
+	switch(type) {
+	case 1:
+		break;
+	case 2:
+		sprintf(value, "%d", size);
+		udev_device_set_sysattr_value(dev, "size", value);
+	default:
+		type = 0;
+		if (monitor)
+			return;
+	}
+
+	if (type == 0)
+		printf("Disable loopback on cport %d\n", cportid);
+	else
+		printf("Enable loopback on cport %d\n", cportid);
+
+	if (ms >= 0) {
+		sprintf(value, "%d", ms);
+		udev_device_set_sysattr_value(dev, "ms_wait", value);
+	}
+
+	sprintf(value, "%d", type);
+	udev_device_set_sysattr_value(dev, "type", value);
+}
+
+void loopback_foreach(struct udev *udev,
+		      struct udev_list_entry *devices,
+		      void (*cb)(struct loopback *, struct udev_device *, int),
+		      struct loopback *loopback)
+{
+	struct udev_device *dev;
+	struct udev_list_entry *dev_list_entry;
+
 	const char *path;
 	const char *devtype;
 	const char *protocol_id;
 
+	int cport = loopback->cport;
+
+	udev_list_entry_foreach(dev_list_entry, devices) {
+		path = udev_list_entry_get_name(dev_list_entry);
+		dev = udev_device_new_from_syspath(udev, path);
+		devtype = udev_device_get_devtype(dev);
+
+		if (strcmp(devtype, "greybus_connection"))
+			continue;
+
+		protocol_id = udev_device_get_sysattr_value(dev,
+							    "protocol_id");
+		if (strcmp(protocol_id, "17"))
+			continue;
+
+		if (cport != -1 && cport != get_cportid(path))
+			continue;
+
+		cb(loopback, dev, get_cportid(path));
+	}
+}
+
+int main (int argc, char *argv[])
+{
+	struct loopback loopback;
+
+	struct udev *udev;
+	struct udev_enumerate *enumerate;
+	struct udev_list_entry *devices, *dev_list_entry;
+	struct udev_device *dev;
+
 	int c;
+
+	memset(&loopback, 0, sizeof(loopback));
+	loopback.cport = -1;
+
 	while ((c = getopt (argc, argv, "t:s:c:mw:")) != -1) {
 		switch (c) {
 		case 'c':
-			if (sscanf(optarg, "%d", &cport) != 1)
+			if (sscanf(optarg, "%d", &loopback.cport) != 1)
 				return -EINVAL;			
 			break;		
 		case 's':
-			if (sscanf(optarg, "%d", &size) != 1)
+			if (sscanf(optarg, "%d", &loopback.size) != 1)
 				return -EINVAL;			
 			break;
 		case 't':
-			if (sscanf(optarg, "%d", &type) != 1)
+			if (sscanf(optarg, "%d", &loopback.type) != 1)
 				return -EINVAL;
 			break;
 		case 'm':
-			monitor = 1;
+			loopback.monitor = 1;
 			break;
 		case 'w':
-			if (sscanf(optarg, "%d", &ms) != 1)
+			if (sscanf(optarg, "%d", &loopback.ms) != 1)
 				return -EINVAL;
 			break;
 		default:
@@ -98,72 +194,23 @@ int main (int argc, char *argv[])
 	udev_enumerate_add_match_subsystem(enumerate, "greybus");
 	udev_enumerate_scan_devices(enumerate);
 	devices = udev_enumerate_get_list_entry(enumerate);
-	udev_list_entry_foreach(dev_list_entry, devices) {		
-		path = udev_list_entry_get_name(dev_list_entry);
-		dev = udev_device_new_from_syspath(udev, path);
-		devtype = udev_device_get_devtype(dev);
 
-		if (strcmp(devtype, "greybus_connection"))
-			continue;
-
-		protocol_id = udev_device_get_sysattr_value(dev, "protocol_id");
-		if (strcmp(protocol_id, "17"))
-			continue;
-
-		if (cport != -1 && cport != get_cportid(path))
-			continue;
-
-		if (!type && monitor)
-			continue;
-
-		if (type == 2) {
-			sprintf(value, "%d", size);
-			udev_device_set_sysattr_value(dev, "size", value);
-		}
-
-		if (type == 0)
-			printf("Disable loopback on cport %d\n", 
-				get_cportid(path));
-		else if (type > 2)
-			return -EINVAL;
-		else
-			printf("Enable loopback on cport %d\n",
-				get_cportid(path));
-		sprintf(value, "%d", type);
-		udev_device_set_sysattr_value(dev, "type", value);
-		if (ms >= 0) {
-			sprintf(value, "%d", ms);
-			udev_device_set_sysattr_value(dev, "ms_wait", value);
-		}
+	if (!devices) {
+		printf("No greybus devices!\n");
+		return -1;
 	}
-	if (monitor) {
+
+	loopback_foreach(udev, devices, loopback_configure, &loopback);
+
+	if (loopback.monitor) {
 		struct timeval ts, te, td;
 		struct timeval tr = {0, 400000 };
 		while (run) {
 			gettimeofday(&ts, NULL);
 			printf("\033[2J");
 			printf("\033[H");
-			udev_list_entry_foreach(dev_list_entry, devices) {
-				path = udev_list_entry_get_name(dev_list_entry);
-				dev = udev_device_new_from_syspath(udev, path);
-				devtype = udev_device_get_devtype(dev);
-
-				if (strcmp(devtype, "greybus_connection"))
-					continue;
-				protocol_id = udev_device_get_sysattr_value(dev, "protocol_id");
-				if (strcmp(protocol_id, "17"))
-					continue;
-				if (cport != -1 && cport != get_cportid(path))
-					continue;
-				printf("cport %d\n", get_cportid(path));
-				printf("\tfreq = %s\n", 					udev_device_get_sysattr_value(dev, "frequency_avg"));
-				printf("\tlatency = %s\n", 
-					udev_device_get_sysattr_value(dev, "latency_avg"));
-				printf("\tbw = %s\n", 
-					udev_device_get_sysattr_value(dev, "throughput_avg"));
-				if (run == 2 && type)
-					udev_device_set_sysattr_value(dev, "type", "0");
-			}
+			loopback_foreach(udev, devices, loopback_monitor,
+					 &loopback);
 			if (run == 2)
 				run = 0;
 			gettimeofday(&te, NULL);
@@ -176,8 +223,7 @@ int main (int argc, char *argv[])
 	}
 
 	udev_enumerate_unref(enumerate);
-
 	udev_unref(udev);
 
-	return 0;       
+	return 0;
 }
